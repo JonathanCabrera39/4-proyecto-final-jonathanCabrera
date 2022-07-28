1f42fc25e690c60a39dd7c25bd7c514e9ec6c18d
jona199608 <subjona@gmail.com>
2022-07-28 14:19:09 -0300

    ultima revicion

--- js/main.js
+++ js/main.js
@@ -65,3 +65,115 @@ function agregarProductosTabla() {
         tabla.append(filaTabla)
     });
 }
+
+// function pintarCarro(){
+//     let aux = "";
+//     for(let i=0;i<productosEnCarro.length;i++){
+//         aux =
+//         aux +`
+//         <article style="border: 1px solid green">
+//         <h3>${productosEnCarro[i].nombre}</h3>
+//         <p>$: ${productosEnCarro[i].precio}</p>
+//         <p>id: ${productosEnCarro[i].id}</p>
+//         <p onclick="borrarDelCarro(${[i]})" style="cursor:pointer;">🗑️</p>
+//         </article>
+//         `;
+//     }
+//     document.getElementById("tablaxl-Productos").innerHTML = aux;
+// }
+
+let ropa = [
+    {id:100, nombre:"Sportswear",precio:200},
+    {id:101, nombre:"Nsw Photo Tee",precio:180},
+    {id:102, nombre:"ordan Stencil Crew",precio:100},
+    {id:103, nombre:"verace logo",precio:100},
+    {id:103, nombre:"MEDUSA BIGGIE",precio:70},
+    {id:104, nombre:"couture t-shit",precio:122},
+    ];
+
+
+
+function meterAlCarro(ObjetoProducto){
+    //console.log(productosEnCarro)
+   productosEnCarro.push(ObjetoProducto);
+   localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+   agregarProductosTabla();
+}
+function borrarDelCarro(id){
+    //productosEnCarro.filter((item => item.id != id));
+    productosEnCarro.splice(id,1)
+    localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+    agregarProductosTabla()
+}
+
+let productosEnCarro;
+
+function pintarListado(){
+    let aux = "";
+    for(let i=0;i<ropa.length;i++){
+        aux =
+        aux +`
+        <article  style= " border: 1px solid red">
+        <h3>nombre: ${ropa[i].nombre}</h3>
+        <p>$: ${ropa[i].precio}</p>
+        <p>id: ${ropa[i].id}</p>
+        <button id="meterAlCarro" onclick="meterAlCarro({id: ${ropa[i].id},nombre: '${ropa[i].nombre}',precio: ${ropa[i].precio}})">agrerar al carro</button>
+        </article>
+        `;
+    }
+    // console.log(aux);
+    document.getElementById("selecionar-producto").innerHTML = aux;
+}
+pintarListado()
+
+
+let aux = localStorage.getItem("productosEnCarro");
+if (!aux) {
+    productosEnCarro = [];
+}else{
+   productosEnCarro = JSON.parse(aux.toString())
+   agregarProductosTabla()
+}
+
+function agregarProductosTabla() {
+    let aux = "";
+    for(let i=0;i<productosEnCarro.length;i++){
+        aux =
+        aux +`
+        <tr style="border: 1px solid green">
+     
+        <td>${productosEnCarro[i].nombre}</td>
+        <td>$: ${productosEnCarro[i].precio}</td>
+        <td>id: ${productosEnCarro[i].id}</td>
+        <td>
+        <td id="borrarDelCarro" onclick="borrarDelCarro(${[i]})" style="cursor:pointer;">🗑️</td>
+        
+        </tr>
+        `;
+    }
+    document.getElementById("tablaxl-Productos").innerHTML = aux;
+}
+
+     
+const btn = document.querySelector("#meterAlCarro")
+btn.addEventListener('click', () => {
+
+    Swal.fire({
+        title: 'Genial!',
+        text: 'Agregaste un producto al carro!',
+        icon: 'success',
+        confirmButtonText: 'Cool'
+})
+})
+
+// const btn2 = document.querySelector('#borrarDelCarro')
+// btn.addEventListener('click', () => {
+
+//     Swal.fire({
+//         title: 'eliminaste!',
+//         text: 'Borraste un producto al carro!',
+//         icon: 'success',
+//         confirmButtonText: 'Cool'
+// })
+// })
+
