5e7f8c0516708471435bbf55ec8e1fb98d46c546
jona199608 <subjona@gmail.com>
2022-08-29 23:46:39 -0300

    completate

--- js/main.js
+++ js/main.js
@@ -1,236 +1,152 @@
-let productos = [];
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
 
-let formulario
-let inputNombre
-let inputPrecioComprar
-let inputColor
-let inputCantidad
-let tabla
-let errores
+document.querySelector(".menu-btn").addEventListener("click", () => {
+document.querySelector(".menu").classList.toggle("show");
+  });
 
-class Producto {
-    constructor(nombre,precioCompra,Color,cantidad){
-        this.nombre = nombre;
-        this.precioCompra = precioCompra;
-        this.Color = Color;
-        this.cantidad = cantidad;
+// Eventos
+// El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
+document.addEventListener('DOMContentLoaded', e => { 
+  fetchData()
+  if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    pintarCarrito()
+}
+ });
+cards.addEventListener('click', e => { addCarrito(e) });
+items.addEventListener('click', e => { btnAumentarDisminuir(e) })
+
+// Traer productos
+const fetchData = async () => {
+    const res = await fetch('data.json');
+    const data = await res.json()
+    // console.log(data)
+    pintarCards(data)
+}
+
+// Pintar productos
+const pintarCards = data => {
+    data.forEach(item => {
+        templateCard.querySelector('h5').textContent = item.nombre
+        templateCard.querySelector('p').textContent = item.precio
+        templateCard.querySelector('img').setAttribute("src",item.img)
+        templateCard.querySelector('button').dataset.id = item.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cards.appendChild(fragment)
+}
+
+// Agregar al carrito
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        // console.log(e.target.dataset.id)
+        // console.log(e.target.parentElement)
+        setCarrito(e.target.parentElement)
     }
+    e.stopPropagation()
 }
 
-function inicilizarElementos(){
-    formulario = document.getElementById("formulario");
-    inputNombre = document.getElementById("nombre")
-    inputPrecioComprar = document.getElementById("precioCompra")
-    inputColor = document.getElementById("Color")
-    inputCantidad = document.getElementById("cantidad")
-    tabla = document.getElementById("tablaProductos")
-    errores= document.querySelector(".errores")
-    errores.style.display = "none"
-}
+const setCarrito = item => {
+    // console.log(item)
+    const producto = {
+        nombre: item.querySelector('h5').textContent,
+        precio: item.querySelector('p').textContent,
+        id: item.querySelector('button').dataset.id,
+        cantidad: 1
+    }
+    // console.log(producto)
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
 
-inicilizarElementos()
-
-
-formulario.onsubmit = (event) =>{
-    event.preventDefault();
+    carrito[producto.id] = { ...producto }
     
-    let nuevoProducto = new Producto(inputNombre.value,inputPrecioComprar.value,inputColor.value,inputCantidad.value)
-    if(inputNombre.value != "" && inputColor.value != "" && inputCantidad.value != "" && inputPrecioComprar.value != ""){
-       productos.unshift(nuevoProducto)
-       limpiarTabla();    
-       agregarProductosTabla()
-       errores.style.display = "none"
-       formulario.reset()
-    }else{
-        errores.style.display = "block"
-    }
+    pintarCarrito()
 }
 
-function limpiarTabla() {
-    while(tabla.rows.length > 1) {
-        tabla.deleteRow(1)
-    }
+const pintarCarrito = () => {
+    items.innerHTML = ''
+
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+        
+        //botones
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    pintarFooter()
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
 
-function agregarProductosTabla() {
-    productos.forEach(element => {
-        let tabla = document.querySelector("#selecionar-producto")
-        let filaTabla = document.createElement("tr")
-        filaTabla.innerHTML=`
-        <td>${element.nombre}</td>
-        <td>${element.precioCompra}</td>
-        <td>${element.Color}</td>
-        <td>${element.cantidad}</td>
+const pintarFooter = () => {
+    footer.innerHTML = ''
+    
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
         `
-        tabla.append(filaTabla)
-    });
-}
-//----------------------------------------------------------------
-// let ropa = [
-//     {id:100, nombre:"Sportswear",precio:200},
-//     {id:101, nombre:"Nsw Photo Tee",precio:180},
-//     {id:102, nombre:"ordan Stencil Crew",precio:100},
-//     {id:103, nombre:"verace logo",precio:100},
-//     {id:103, nombre:"MEDUSA BIGGIE",precio:70},
-//     {id:104, nombre:"couture t-shit",precio:122},
-//     ];
-
-// let color = [
-//     {red:"red", },
-//     {verde:"verde",},
-//     {azul:"azul"},
-//     {violeta:"violeta"},
-//     {amarrillo:"amarrillo"},
-//     {negro:"negro"},
-//     ];
-
-  // function selecionarColor(){
-    //     let aux = "";
-    //     for(let i=0;i<color.length;i++){
-    //         aux =
-    //         aux +
-    //         `
-    //         <article  style= " border: 1px solid red">
-    //         <button id="" onclick="({id: ${color[i].red}})">${red}</button>
-    //         <button id="" onclick="({id: ${color[i].verde}})">${verde}</button>
-    //         <button id="" onclick="({id: ${color[i].amarrillo}})">${amarrillo}</button>
-    //         <button id="" onclick="({id: ${color[i].azul}})">${azul}</button>
-    //         <button id="" onclick="({id: ${color[i].violeta}})">${violeta}</button>
-    //         <button id="" onclick="({id: ${color[i].negro}})">${negro}</button>
-    //         </article>
-    //         `;
-    //     }
-    //     // console.log(aux);
-    //     document.getElementById("selecionar-color").innerHTML = aux;
-    // }
+        return
+    }
     
-    function meterAlCarro(ObjetoProducto){
-        //console.log(productosEnCarro)
-        productosEnCarro.push(ObjetoProducto);
-        localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
-        agregarProductosTabla();
-}
-function borrarDelCarro(id){
-    //productosEnCarro.filter((item => item.id != id));
-    productosEnCarro.splice(id,1)
-    localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
-    agregarProductosTabla()
+    // sumar cantidad y sumar totales
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
+    // console.log(nPrecio)
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+
+    footer.appendChild(fragment)
+
+    const boton = document.querySelector('#vaciar-carrito')
+    boton.addEventListener('click', () => {
+        carrito = {}
+        pintarCarrito()
+    })
+
 }
 
+const btnAumentarDisminuir = e => {
+    // console.log(e.target.classList.contains('btn-info'))
+    if (e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = { ...producto }
+        pintarCarrito()
+    }
 
-// function pintarListado(){
-//     let aux = "";
-//     for(let i=0;i<ropa.length;i++){
-//         aux =
-//         aux +`
-//         <article  style= " border: 1px solid red">
-//         <h3>nombre: ${ropa[i].nombre}</h3>
-//         <p>$: ${ropa[i].precio}</p>
-//         <p>id: ${ropa[i].id}</p>
-//         <button id="meterAlCarro" onclick="meterAlCarro({id: ${ropa[i].id},nombre: '${ropa[i].nombre}',precio: ${ropa[i].precio}})">agrerar al carro</button>
-//         </article>
-//         `;
-//     }
-//     // console.log(aux);
-//     document.getElementById("selecionar-producto").innerHTML = aux;
-// }
-async function pintarListado() {
-    const ropa = await fetch('./data.json')
-     productosEnCarro = await ropa.json();
-        let aux = '';
-        for (let i = 0; i < productosEnCarro.length; i++) {
-          aux =
-            aux +
-            `
-            <article  style= " border: 1px solid red">
-            <p>$: ${productosEnCarro[i].precio}</p>
-            <h3>nombre:${productosEnCarro[i].nombre}</h3>
-                <p>id: ${productosEnCarro[i].id}</p>
-                <button id="meterAlCarro" onclick="meterAlCarro({id: ${productosEnCarro[i].id},nombre: '${productosEnCarro[i].nombre}',precio: ${productosEnCarro[i].precio}})">agrerar al carro</button>
-            </article>
-            `;
+    if (e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        } else {
+            carrito[e.target.dataset.id] = {...producto}
         }
-        document.getElementById('selecionar-producto').innerHTML = aux;
-        console.log(aux);
-      }
-pintarListado()
-
-let aux = localStorage.getItem("productosEnCarro");
-let productosEnCarro;
-
-productosEnCarro = (JSON.parse(aux.toString()) || [])
-agregarProductosTabla()
-
-// if (!aux) {
-//     productosEnCarro = [];
-// }else{
-    //    productosEnCarro = JSON.parse(aux.toString())
-    //    agregarProductosTabla()
-    // }
-    
-    // function agregarProductosTabla() {
-    //     let aux = "";
-    //     for(let i=0;i<productosEnCarro.length;i++){
-    //         aux =
-    //         aux +`
-    //         <tr style="border: 1px solid green">
-            
-    //         <td>${productosEnCarro[i].nombre}</td>
-    //         <td>$: ${productosEnCarro[i].precio}</td>
-    //         <td>id: ${productosEnCarro[i].id}</td>
-    //         <td>id: ${productosEnCarro[i].id}</td>
-    //         <td><button id="borrarDelCarro" onclick="borrarDelCarro(${[i]})" style="cursor:pointer;">🗑️</button></td>
-    //         </tr>
-    //         `;
-    //     }
-    //     document.getElementById("tablaxl-Productos").innerHTML = aux;
-    // }
-    async function agregarProductosTabla() {
-        const ropa = await fetch('./data.json')
-         productosEnCarro = await ropa.json();
-            let aux = '';
-            for (let i = 0; i < productosEnCarro.length; i++) {
-              aux =
-                aux +
-                `
-            <tr style="border: 1px solid green">
-            <td>${productosEnCarro[i].nombre}</td>
-            <td>$: ${productosEnCarro[i].precio}</td>
-            <td>id: ${productosEnCarro[i].id}</td>
-            <td>id: ${productosEnCarro[i].id}</td>
-            <td><button id="borrarDelCarro" onclick="borrarDelCarro(${[i]})" style="cursor:pointer;">🗑️</button></td>
-            </tr>
-            `;
-            }
-            document.getElementById('tablaxl-Productos').innerHTML = aux;
-            console.log(aux);
-          }
-          
-   // selecionarColor()
-    //------------------------------------------------------------------------------
-
-// let btn = document.querySelector("#meterAlCarro")
-// btn.addEventListener('click', () => {
-//         Swal.fire({
-//             allowOutsideClick:false,
-//             title: 'Genial!',
-//             text: 'Agregaste un producto al carro!',
-//             icon: 'success',
-//             confirmButtonText: 'Cool',
-//         })
-//         });
-
-// const btn2 = document.querySelector('#borrarDelCarro')
-// btn2.addEventListener('click', () => {
-
-//     Swal.fire({
-//         allowOutsideClick:false,
-//         title: 'eliminaste!',
-//         text: 'Borraste un producto al carro!',
-//         icon: 'success',
-//         confirmButtonText: 'Cool',
-// })
-// });
+        pintarCarrito()
+    }
+    e.stopPropagation()
+}
 
 (async () =>{
     const {value: pais} = await Swal.fire({
@@ -242,11 +158,14 @@ agregarProductosTabla()
         footer:'<span class="colores"> esta infomacion es importante </span>',
         width:'90%',
         padding: '1rem',
-        background: 'linear-gradient(90deg, rgba(215,143,183,1) 0%, rgba(201,185,241,1) 100%)',
+        background: 'var(--purpledark)',
+        color: 'var(--whiter)',
+
         //grow:'column'
         backdrop: 'true',
         timer:2*5000,
         timerProgressBar: 'auto',
+       
         //toast:'true',
         position:'center',
         allowOutsideClick:false,
@@ -280,9 +199,9 @@ agregarProductosTabla()
           //}
       	
     
-        // showConfirmButton:
-        // confirmButtonColor:
-        // confirmButtonAriaLabel:
+        showConfirmButton:'var(--purpledark)',
+        confirmButtonColor:'var(--purpledark)',
+        confirmButtonAriaLabel:'var(--purpledark)',
     
         // showCancelButton:
         // cancelButtonText:
@@ -303,16 +222,22 @@ agregarProductosTabla()
     if(pais){
         Swal.fire({
             title: `seleccionaste ${pais}`,
+            background: 'var(--purpledark)',
+            color: 'var(--whiter)',
+            confirmButtonColor:'var(--purpledark)',
+            confirmButtonAriaLabel:'var(--purpledark)',
            
         });
     }else{
         Swal.fire({
             title: `sele acabo el tiempo`,
         footer:'<span class="colores"> actualize la pagina para seleccionar su pais </span>',
+        background: 'var(--purpledark)',
+        color: 'var(--whiter)',
+        confirmButtonColor:'var(--purpledark)',
+        confirmButtonAriaLabel:'var(--purpledark)',
+
 
         }); 
     }
 })()
-
-
-
